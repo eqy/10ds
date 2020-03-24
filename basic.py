796b4bec8ae8b95ce426365ee8b504e48f67c00b
Eddie Yan <Loser777@gmail.com>
2020-03-23 19:18:02 -0700

    download reddit

--- basic.py
+++ basic.py
@@ -1,13 +1,39 @@
+import json
 import os
+import requests
+
 
 from alpha_vantage.timeseries import TimeSeries
+
+def get_wsb(reqs=10):
+    after = None
+    children = list()
+    for req in range(reqs):
+        print("getting req", req)
+        url = 'https://www.reddit.com/r/wallstreetbets.json'
+        if after is not None:
+            url = url + '?after=' + after
+        response = requests.get(url,
+                                headers={'User-agent': '10ds'}).json()
+        after = response['data']['after'] 
+        children += response['data']['children']
+    return children
+
+
+def get_post_text(child):
+    return child['data']['selftext']
 
 
 def main():
     alpha_vantage_key = os.environ['ALPHA_VANTAGE_KEY']
     series = TimeSeries(alpha_vantage_key)
     aapl, meta = series.get_daily(symbol='AAPL')
-    print(aapl)
+    wsb = get_wsb()
+    for child in wsb:
+        print(get_post_text(child))
+        print("--------------")
+    
+
 
 if __name__ == '__main__':
     main()
