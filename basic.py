668d2cd92815c138adab8f027f4cde4f76ef127b
Eddie Yan <Loser777@gmail.com>
2020-03-24 02:03:05 -0700

    meme regex

--- basic.py
+++ basic.py
@@ -1,9 +1,34 @@
 import json
 import os
+import re
 import requests
+import time
 
 
 from alpha_vantage.timeseries import TimeSeries
+
+#def has_strike(child):
+#    res = re.search('[0-9]{1,5}(p|c)', child['data']['selftext'])
+#    if res != None:
+#        print("??", res)
+#    return res != None
+#
+#
+#def has_ticker(child):
+#    crap = {'TLDR', 'DD', 'IV'}
+#    res = re.search('[A-Z]{2,4}', child['data']['selftext'])
+#    print("?", res)
+#    if res == None:
+#        return False
+#    else:
+#        for res in 
+#    return False
+
+def has_meme(child):
+    res = re.search('[A-Z]{2,4} +[0-9]{1,5}(p|c)', child['data']['selftext'])
+    if res != None:
+        print(res)
+    return res != None 
 
 def get_wsb(reqs=10):
     after = None
@@ -17,6 +42,7 @@ def get_wsb(reqs=10):
                                 headers={'User-agent': '10ds'}).json()
         after = response['data']['after'] 
         children += response['data']['children']
+        time.sleep(0.5)
     return children
 
 
@@ -28,10 +54,11 @@ def main():
     alpha_vantage_key = os.environ['ALPHA_VANTAGE_KEY']
     series = TimeSeries(alpha_vantage_key)
     aapl, meta = series.get_daily(symbol='AAPL')
-    wsb = get_wsb()
+    wsb = get_wsb(50)
     for child in wsb:
-        print(get_post_text(child))
-        print("--------------")
+        if has_meme(child):
+            print(get_post_text(child))
+            print("--------------")
     
 
 
