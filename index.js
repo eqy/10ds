9d0f5c020b9aa8ad8a11621afc819d397d25cce8
Eddie Yan <Loser777@gmail.com>
2020-03-29 16:51:54 -0700

    brain_smoothness++;

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function OAuthInit(callback) {
 
 
 /* smol brain */
-let invalid_memes = new Set(['ROPE', 'LIFE', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUNE', 'JUL', 'JULY', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'OR', 'AT', 'MY', 'THE', 'AND', 'OF', 'OWN', 'BUY', 'HOLD', 'MORE', 'YOUR', 'OTM']);
+let invalid_memes = new Set(['ROPE', 'LIFE', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUNE', 'JUL', 'JULY', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'OR', 'AT', 'MY', 'THE', 'AND', 'OF', 'OWN', 'BUY', 'HOLD', 'MORE', 'YOUR', 'OTM', 'SELL', 'SOLD']);
 
 function findMeme(child) {
     /* cutting-edge state-of-the-art sentiment analysis */
